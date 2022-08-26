ba27f6b5d528adfbb1b72e641db46fc9c6678659
Kevin Bertrand <k.bertrand@desyntic.com>
2022-08-26 13:31:55 +0200

    Adding estimate controller

---
+++ Sources/App/Controllers/EstimateController.swift
@@ -0,0 +1,22 @@
+//
+//  EstimateController.swift
+//  
+//
+//  Created by Kevin Bertrand on 26/08/2022.
+//
+
+import Fluent
+import Vapor
+
+struct EstimateController: RouteCollection {
+    // MARK: Route initialisation
+    func boot(routes: RoutesBuilder) throws {
+        
+    }
+    
+    // MARK: Routes functions
+    
+    
+    // MARK: Utilities functions
+    
+}
