3adb46358ee09842ef1b5698347c0e0738224a36
Kevin Bertrand <k.bertrand@desyntic.com>
2022-08-26 17:56:55 +0200

    Adding Getting invoice reference

--- Sources/App/Controllers/InvoiceController.swift
+++ Sources/App/Controllers/InvoiceController.swift
@@ -14,12 +14,61 @@ struct InvoiceController: RouteCollection {
     
     // MARK: Route initialisation
     func boot(routes: RoutesBuilder) throws {
+        let invoiceGroup = routes.grouped("invoice")
         
+        let tokenGroup = invoiceGroup.grouped(UserToken.authenticator()).grouped(UserToken.guardMiddleware())
+        tokenGroup.get("reference", use: getInvoiceReference)
     }
     
     // MARK: Routes functions
+    /// Getting new invoice reference
+    private func getInvoiceReference(req: Request) async throws -> Response {
+        let invoices = try await Invoice.query(on: req.db).all()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMM"
+        let date = dateFormatter.string(from: Date())
+        
+        var number: String = "001"
+        
+        if invoices.count != 0 {
+            let lastReference = invoices[invoices.count-1].reference.split(separator: "-")
+            guard lastReference.count == 3 else { throw Abort(.internalServerError) }
+            
+            if lastReference[1] == date {
+                guard let lastNumber = Int(lastReference[2]) else { throw Abort(.internalServerError) }
+                let newNumber = "\(lastNumber+1)"
+                number = ""
+                
+                for _ in 0..<(3-newNumber.count) { number.append("0")}
+                
+                number.append(newNumber)
+            }
+        }
+
+        return formatResponse(status: .ok, body: try encodeBody("F-\(date)-\(number)"))
+    }
     
+    /// Create invoice
+    /// Update invoice
+    /// Getting invoice list
+    /// Getting invoice
     
     // MARK: Utilities functions
+    /// Getting the connected user
+    private func getUserAuthFor(_ req: Request) throws -> Staff {
+        return try req.auth.require(Staff.self)
+    }
     
+    /// Formating response
+    private func formatResponse(status: HTTPResponseStatus, body: Response.Body) -> Response {
+        var headers = HTTPHeaders()
+        headers.add(name: .contentType, value: "application/json")
+        return .init(status: status, headers: headers, body: body)
+    }
+    
+    /// Encode body
+    private func encodeBody(_ body: Codable) throws -> Response.Body {
+        return .init(data: try JSONEncoder().encode(body))
+    }
 }
