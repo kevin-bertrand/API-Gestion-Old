841b5b1ac71e8733c24e12ab4e071c635576bbe0
Kevin Bertrand <k.bertrand@desyntic.com>
2022-08-26 13:12:03 +0200

    Adding Payment controller

---
+++ Sources/App/Controllers/PaymentController.swift
@@ -0,0 +1,22 @@
+//
+//  PaymentController.swift
+//  
+//
+//  Created by Kevin Bertrand on 26/08/2022.
+//
+
+import Fluent
+import Vapor
+
+struct PaymentController: RouteCollection {
+    // MARK: Route initialisation
+    func boot(routes: RoutesBuilder) throws {
+        
+    }
+    
+    // MARK: Routes functions
+    
+    
+    // MARK: Utilities functions
+    
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,4 +7,5 @@ func routes(_ app: Application) throws {
     try app.register(collection: StaffController(addressController: addressController))
     try app.register(collection: ProductController())
     try app.register(collection: ClientController(addressController: addressController))
+    try app.register(collection: PaymentController())
 }
