7b98ffdd12dc9738b512e8d40eb20d176a4eb1cd
Kevin Bertrand <k.bertrand@desyntic.com>
2022-08-26 14:57:40 +0200

    Adding "getting new estimate reference"

--- Sources/App/Controllers/EstimateController.swift
+++ Sources/App/Controllers/EstimateController.swift
@@ -14,11 +14,58 @@ struct EstimateController: RouteCollection {
         let estimateGroup = routes.grouped("estimate")
                 
         let tokenGroup = estimateGroup.grouped(UserToken.authenticator()).grouped(UserToken.guardMiddleware())
+        tokenGroup.get("reference", use: getEstimateReference)
         tokenGroup.post("add", use: create)
     }
     
     // MARK: Routes functions
     /// Getting new estimate reference
+    private func getEstimateReference(req: Request) async throws -> Response {
+        let estimates = try await Estimate.query(on: req.db)
+            .all()
+        
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy"
+        let yearString = dateFormatter.string(from: currentDate)
+        dateFormatter.dateFormat = "MM"
+        let monthString = dateFormatter.string(from: currentDate)
+        let date = yearString + monthString
+        
+        var number: String = ""
+        
+        if estimates.count == 0 {
+            number = "001"
+        } else {
+            let lastReference = estimates[estimates.count-1].reference.split(separator: "-")
+            print(lastReference)
+            
+            guard lastReference.count == 3 else { throw Abort(.internalServerError) }
+            
+            if lastReference[1] != date {
+                number = "001"
+            } else {
+                guard let lastNumber = Int(lastReference[2]) else {
+                    print("error here")
+                    throw Abort(.internalServerError)
+                }
+                let newNumber = "\(lastNumber+1)"
+                
+                for _ in 0..<(3-newNumber.count) {
+                    number.append("0")
+                }
+                
+                number.append(newNumber)
+            }
+        }
+        
+        var reference: String = "D-"
+        reference.append(date)
+        reference.append("-")
+        reference.append(number)
+        
+        return formatResponse(status: .ok, body: try encodeBody(reference))
+    }
     
     /// Create
     private func create(req: Request) async throws -> Response {
