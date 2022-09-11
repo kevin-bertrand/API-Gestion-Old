65f361d6d8fd749fe9897b691d026ac17d0eef12
Kevin Bertrand <k.bertrand@desyntic.com>
2022-09-11 16:12:18 +0200

    Modify deprecated variables on Export to PDF function

--- Sources/App/PDF/Document.swift
+++ Sources/App/PDF/Document.swift
@@ -51,55 +51,6 @@ public class Document {
 }
 
 extension Document {
-
-    public func generatePDF(on threadPool: NIOThreadPool, eventLoop: EventLoop) -> EventLoopFuture<Data> {        
-        return threadPool.runIfActive(eventLoop: eventLoop) {
-            let fileManager = FileManager.default
-
-            // Create the temp folder if it doesn't already exist
-            let workDir = "/tmp/vapor-wkhtmltopdf"
-            try fileManager.createDirectory(atPath: workDir, withIntermediateDirectories: true)
-
-            // Save input pages to temp files, and build up args to wkhtmltopdf
-            var wkArgs: [String] = [
-                "--zoom", self.zoom,
-                "--quiet",
-                "--footer-center", "'[page]/[topage]'",
-                "-s", self.paperSize,
-                "-T", "\(self.topMargin)mm",
-                "-R", "\(self.rightMargin)mm",
-                "-B", "\(self.bottomMargin)mm",
-                "-L", "\(self.leftMargin)mm",
-            ]
-            
-            wkArgs += self.wkArgs
-            
-            let pageFiles: [String] = try self.pages.map { page in
-                let name = UUID().uuidString + ".html"
-                let filename = "\(workDir)/\(name)"
-                try page.content.write(to: URL(fileURLWithPath: filename))
-                return filename
-            }
-            defer {
-                try? pageFiles.forEach(fileManager.removeItem)
-            }
-
-            wkArgs += pageFiles
-            
-            // Call wkhtmltopdf and retrieve the result data
-            let wk = Process()
-            let stdout = Pipe()
-            wk.launchPath = self.launchPath
-            wk.arguments = wkArgs
-            wk.arguments?.append("-") // output to stdout
-            wk.standardOutput = stdout
-            wk.launch()
-            
-            let pdf = stdout.fileHandleForReading.readDataToEndOfFile()
-            return pdf
-        }
-    }
-    
     public func generatePDF(on threadPool: NIOThreadPool, eventLoop: EventLoop, title: String) async throws -> Data {
         return try threadPool.runIfActive(eventLoop: eventLoop) {
             let fileManager = FileManager.default
@@ -138,11 +89,11 @@ extension Document {
             // Call wkhtmltopdf and retrieve the result data
             let wk = Process()
             let stdout = Pipe()
-            wk.launchPath = self.launchPath
+            wk.executableURL = URL(string: self.launchPath)
             wk.arguments = wkArgs
             wk.arguments?.append("-") // output to stdout
             wk.standardOutput = stdout
-            wk.launch()
+            try wk.run()
             
             let pdf = stdout.fileHandleForReading.readDataToEndOfFile()
             return pdf
