b3a13402f0b8b43256a3abf4bdffcaa7b99718d8
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2022-09-21 18:27:11 +0200

    Update CronJobs

--- CronJobs.py
+++ CronJobs.py
@@ -1,31 +1,62 @@
 #!/usr/bin/python3
+###############################################
+## Librairies import
+###############################################
 import psycopg2
 from decouple import config
+from datetime import date
 
+###############################################
+## Getting environment variables
+###############################################
 DATABASE_HOST = config('DATABASE_HOST')
 DATABASE_PORT = config('DATABASE_PORT')
 DATABASE_USERNAME = config('DATABASE_USERNAME')
 DATABASE_PASSWORD = config('DATABASE_PASSWORD')
 DATABASE_NAME = config('DATABASE_NAME')
 
-# Connect to the Database
+###############################################
+## Configure DB
+###############################################
 conn = psycopg2.connect("host=%s port=%s dbname=%s user=%s password=%s" % (DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD))
-print(conn)
-
-# Open a cursor to send SQL Commands
 cur = conn.cursor()
 
-# Select all invoices
-sql = "SELECT * FROM invoice WHERE status='sent';"
-cur.execute(sql)
-invoices = cur.fetchall()
+###############################################
+## Functions
+###############################################
+# Get formated date
+def GetFormatedDate(date):
+    date = date.split('/')
+    return datetime.datetime(date[2], date[1], date[0])
 
-for invoice in invoices:
-    reference = invoice[1]
-    limit = invoice[13]
-    print(reference)
-    print(limit)
+# Update invoice
+def UpdateInvoice(reference, limit):
+    today = date.today()
+
+    if today < GetFormatedDate(limit):
+        print("ok")
+    else:
+        print("not ok")
+
+# Select all invoices
+def SelectInvoices():
+    sql = "SELECT * FROM invoice WHERE status='sent';"
+    cur.execute(sql)
+    invoices = cur.fetchall()
+
+    for invoice in invoices:
+        UpdateInvoice(invoice[1], invoice[13])
+
+
+
+###############################################
+## Main
+###############################################
+SelectInvoices()
+
+conn.close()
+
+
         
 # Close connection
-conn.close()
-print(conn)
+
