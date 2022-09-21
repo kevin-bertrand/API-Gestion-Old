48da069d4f9d07f997ecaffa9a0fb6f9a7282c3e
Kevin Bertrand <73759450+kevin-bertrand@users.noreply.github.com>
2022-09-21 16:10:00 +0200

    Improve Redis for server

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -52,12 +52,11 @@ public func configure(_ app: Application) throws {
     app.migrations.add(DefaultAdministratorMigration())
     
     // Configure jobs
-//
-    let configuration = try RedisConfiguration(hostname: "127.0.0.1",
-                                               port: 2575,
-                                               password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
-                                               pool: .init(connectionRetryTimeout: .milliseconds(60)))
-    app.queues.use(.redis(configuration))
+    app.redis.configuration = try RedisConfiguration(hostname: "127.0.0.1",
+                                                     port: 2575,
+                                                     password: "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81",
+                                                     pool: .init(connectionRetryTimeout: .milliseconds(60)))
+    try app.queues.use(.redis(url: "redis://127.0.0.1:2575"))
     app.queues.schedule(InvoiceStatusJob())
         .minutely()
         .at(0)
